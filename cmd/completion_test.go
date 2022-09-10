81ace0b6ab0f1be1a729ddbbdcc9bb7e05976cd2
purpleclay <purpleclaygh@gmail.com>
2022-09-10 13:21:10 +0100

    fix sonarcloud linting issues

--- cmd/completion_test.go
+++ cmd/completion_test.go
@@ -30,7 +30,7 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-func TestCompletion_Bash(t *testing.T) {
+func TestCompletionBash(t *testing.T) {
 	var buf bytes.Buffer
 	cmd := newCompletionCmd(&buf)
 	cmd.SetArgs([]string{"bash"})
@@ -42,7 +42,7 @@ func TestCompletion_Bash(t *testing.T) {
 	assert.Contains(t, buf.String(), "bash completion V2 for completion ")
 }
 
-func TestCompletion_Zsh(t *testing.T) {
+func TestCompletionZsh(t *testing.T) {
 	var buf bytes.Buffer
 	cmd := newCompletionCmd(&buf)
 	cmd.SetArgs([]string{"zsh"})
@@ -54,7 +54,7 @@ func TestCompletion_Zsh(t *testing.T) {
 	assert.Contains(t, buf.String(), "zsh completion for completion")
 }
 
-func TestCompletion_ZshNoDescriptions(t *testing.T) {
+func TestCompletionZshNoDescriptions(t *testing.T) {
 	var buf bytes.Buffer
 	cmd := newCompletionCmd(&buf)
 	cmd.SetArgs([]string{"zsh", "--no-descriptions"})
@@ -67,7 +67,7 @@ func TestCompletion_ZshNoDescriptions(t *testing.T) {
 	assert.Contains(t, buf.String(), "__completeNoDesc")
 }
 
-func TestCompletion_Fish(t *testing.T) {
+func TestCompletionFish(t *testing.T) {
 	var buf bytes.Buffer
 	cmd := newCompletionCmd(&buf)
 	cmd.SetArgs([]string{"fish"})
